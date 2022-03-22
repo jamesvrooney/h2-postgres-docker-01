b267a2c47a7260ded1ff78824ef65aebc0d7e8e7
jamesvrooney <jamesvrooney@hotmail.com>
2022-03-22 06:23:47 +0000

    Initial commit.

---
+++ src/main/java/com/jamesvrooney/H2PostgresDockerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jamesvrooney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2PostgresDockerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2PostgresDockerExampleApplication.class, args);
+	}
+
+}
